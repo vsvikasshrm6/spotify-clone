4ec50a65005659cc1406d88d7632735ed23781a3
Vikas Sharma <vsvikasshrm6@gmail.com>
2025-09-23 17:21:11 +0530

    deleteSong

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,10 +1,33 @@
+import { Song } from './../models/song.model';
+import cloudinary from '../utils/cloudinary';
+
 export const createSong = async(req, res)=>{
   try {
     if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
     }
+    const {title, duration,artist, album  } = req.body;
+    const audioFile = req.files.audioFile;
+    const imageFile = req.files.imageFile;
+    if(!title || !duration || !artist || !audioFile || !imageFile){
+      return res.status(500).json({message : "Please provide all details"});
+    }
+    const audioUrl = (await cloudinary.uploader.upload(audioFile)).secure_url;
+    const imageUrl = (await cloudinary.uploader.upload(imageFile)).secure_url;
+
+
+    await Song.create({
+      title,
+      duration,
+      artist,
+      album : (album) ? album : null,
+      imageUrl,
+      audioUrl
+    })
     
   } catch (error) {
-    
+    console.log(error);
+    res.status(500).json({error : "Error in uploading song"})
   }
 }
+//delete song , create album , delete album
---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudinary} from "cloudinary"
+
+cloudinary.config({ 
+  cloud_name: 'dntvkwmud', 
+  api_key: '916826785941833', 
+  api_secret: 'SlCMn0B7T8Cy235mUaiYvhp0iG4'
+});
+
+export default cloudinary;
