ec508547dd027a1e48d179d472b357b3c1dc174c
vsvikashrm6 <vsvikasshrm6@gmail.com>
2025-11-01 13:58:28 +0530

    StatsControllerDone

--- backend/src/controller/stats.controller.js
+++ backend/src/controller/stats.controller.js
@@ -8,14 +8,36 @@ export async function getAllStats(req, res) {
     // const albumCount = await Song.countDocuments();
     // const userCount = await User.countDocuments();
 
-    const [songsCount, albumCount, userCount] = await Promise.all([
+    const [songsCount, albumCount, userCount, uniqueArtist] = await Promise.all([
       Song.countDocuments(),
       Album.countDocuments(),
-      User.countDocuments()
+      User.countDocuments(),
+      Song.aggregate([{
+        $unionWith : {
+          coll: "Album",
+          pipeline : [],
+        },
+        
+      
+      },
+      {
+        $group : {
+          _id : "$artist",
+        }
+      },
+      {
+        $count : "count" 
+      }
+    ])
     ])
 
+    res.status(200).json({
+      albumCount, userCount, songsCount, totalArtist : uniqueArtist[0]?.count || 0,
+    })
+
   } catch (error) {
-    
+    console.log("Error in getting All Stats");
+    res.status(500).json({error : "Internal Server Error"});
   }
   
 }
